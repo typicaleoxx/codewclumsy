85ddd5622c868e89472d02f91e344fa25f5f9da6
typicaleoxx <lsneha991@gmail.com>
2024-01-23 16:11:42 +0545

    changed bg color

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
     const navLinks = document.querySelector('.nav-links');
 
     navToggle.addEventListener('click', function () {
-        navLinks.style.display = navLinks.style.display === 'none' ? 'flex' : 'none';
+        navLinks.classList.toggle('show');
     });
 });
